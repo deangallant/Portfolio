0c210264adbd342b5b3d12840e41afe83d06a2a2
deangallant <deangallant@student.mitt.ca>
2021-05-21 16:33:51 -0500

    disable SM on JS (Crypto/MD)

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,9 @@ function copyIframes() {
 
 function copyJS() {
   return src('src/js/*')
-  .pipe(sourceMaps.init())
+  //.pipe(sourceMaps.init())
   //.pipe(uglify())
-  .pipe(sourceMaps.write())
+  //.pipe(sourceMaps.write())
   .pipe(dest('dist/js/'))
 }
 
