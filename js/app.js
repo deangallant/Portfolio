7d73188161830a8f57962a2de7dee7a30e1b190e
deangallant <deangallant@student.mitt.ca>
2021-05-21 08:11:13 -0500

    remote dist in .gitignore to publish via GH-pages

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const navButtons = document.querySelectorAll('.navButton')
+const content = document.querySelector('.content')
+
+navButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    console.log(e.target.innerHTML)
+    loadIframe(e.target.innerHTML)
+  })
+})
+
+
+
+function loadIframe(name) {
+  content.innerHTML = ''
+  content.insertAdjacentHTML("afterbegin",`
+  <iframe src="Portfolio Examples/${name}/index.html" height='100%' width="100%"   frameborder="0"></iframe>
+  `
+  )
+}
