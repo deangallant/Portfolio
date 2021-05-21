df87250a0afc42654ccfd168507e91728214f96d
deangallant <deangallant@student.mitt.ca>
2021-05-20 22:18:53 -0500

    add copyJS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 const { src, dest, series, parallel, watch} = require('gulp');
-
+const replaceHTML = require('gulp-html-replace')
 
 function copyHTML() {
   return src('src/*.html')
   .pipe(dest('dist/'))
 }
 
-
 function copyIframes() {
   return src('src/Portfolio Examples/**/*')
   .pipe(dest('dist/Portfolio Examples/'));
 }
 
+function copyJS() {
+  return src('src/js/*')
+  .pipe(dest('dist/js/'))
+}
+
 
 exports.default = series(copyHTML)
 exports.copyIframes = copyIframes
+exports.copyJS = copyJS
